172399a6e41927c318075e618a703a0bfb2dbccb
Marat Oshmiansky <maratical@gmail.com>
2021-06-18 08:49:08 -0700

    fix controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,17 +1,15 @@
 class MoviesController < ApplicationController
-  def movie_params
-    input_value = params["movie"]
-    render json: { message: "The message is #{input_value}." }
-  end
+  # def movie_params
+  #   input_value = params["movie"]
+  #   render json: { message: "The message is #{input_value}." }
+  # end
 
   def one_movie
     input_value = params["title"]
-    render json: { message: Movie.find_by(title: input_value) }
+    render json: Movie.find_by(title: input_value)
   end
 
   def all_movies
-    movies = Movie.all
-    p movies
-    # render json: { message: Movie.find_by(title: input_value) }
+    render json: Movie.all
   end
 end
