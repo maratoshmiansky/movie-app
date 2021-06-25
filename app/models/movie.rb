e7128ba1d65b22656b284634377997c534992cd5
Marat Oshmiansky <maratical@gmail.com>
2021-06-25 08:07:08 -0700

    add validations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  validates :title, length: { minimum: 2 }
+  validates :year, numericality: { only_integer: true, greater_than: 1888 }
+  validates :plot, length: { minimum: 3 }
+  validates :director, length: { minimum: 2 }
 end
