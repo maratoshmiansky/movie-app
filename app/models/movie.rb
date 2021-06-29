c4b3eac9e1610ce0cd4f5a38c8d2f9f6a547f0eb
Marat Oshmiansky <maratical@gmail.com>
2021-06-29 08:44:50 -0700

    add has many and belongs to associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :actors  # returns array of multiple actors
+
   validates :title, length: { minimum: 2 }
   validates :year, numericality: { only_integer: true, greater_than: 1888 }
   validates :plot, length: { minimum: 3 }
