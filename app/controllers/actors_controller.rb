d5359030db3e0c9224cd16782e8dead935b46b70
Marat Oshmiansky <maratical@gmail.com>
2021-07-02 01:25:49 -0700

    update actors controller with movie_id; change strings to symbols in actors and movies controllers

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,19 +1,20 @@
 class ActorsController < ApplicationController
   def index
     actors = Actor.all
-    render json: actors.as_json
+    render json: actors
   end
 
   def create
     actor = Actor.new(
-      first_name: params["first_name"],
-      last_name: params["last_name"],
-      known_for: params["known_for"],
-      gender: params["gender"],
-      age: params["age"],
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for],
+      gender: params[:gender],
+      age: params[:age],
+      movie_id: params[:movie_id],
     )
     if actor.save
-      render json: actor.as_json
+      render json: actor
     else
       render json: { errors: actor.errors.full_messages }, status: :unprocessable_entity
     end
@@ -21,18 +22,19 @@ class ActorsController < ApplicationController
 
   def show
     actor = Actor.find(params[:id])
-    render json: actor.as_json
+    render json: actor
   end
 
   def update
     actor = Actor.find(params[:id])
-    actor.first_name = params["first_name"] || actor.first_name
-    actor.last_name = params["last_name"] || actor.last_name
-    actor.known_for = params["known_for"] || actor.known_for
-    actor.gender = params["gender"] || actor.gender
-    actor.age = params["age"] || actor.age
+    actor.first_name = params[:first_name] || actor.first_name
+    actor.last_name = params[:last_name] || actor.last_name
+    actor.known_for = params[:known_for] || actor.known_for
+    actor.gender = params[:gender] || actor.gender
+    actor.age = params[:age] || actor.age
+    actor.movie_id = params[:movie_id] || actor.movie_id
     if actor.save
-      render json: actor.as_json
+      render json: actor
     else
       render json: { errors: actor.errors.full_messages }, status: :unprocessable_entity
     end
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,19 @@
 class MoviesController < ApplicationController
   def index
     movies = Movie.all
-    render json: movies.as_json
+    render json: movies
   end
 
   def create
     movie = Movie.new(
-      title: params["title"],
-      year: params["year"],
-      plot: params["plot"],
-      director: params["director"],
-      english: params["english"],
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot],
+      director: params[:director],
+      english: params[:english],
     )
     if movie.save
-      render json: movie.as_json
+      render json: movie
     else
       render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
     end
@@ -21,18 +21,18 @@ class MoviesController < ApplicationController
 
   def show
     movie = Movie.find(params[:id])
-    render json: movie.as_json
+    render json: movie
   end
 
   def update
     movie = Movie.find(params[:id])
-    movie.title = params["title"] || movie.title
-    movie.year = params["year"] || movie.year
-    movie.plot = params["plot"] || movie.plot
-    movie.director = params["director"] || movie.director
-    movie.english = params["english"] || movie.english
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+    movie.director = params[:director] || movie.director
+    movie.english = params[:english] || movie.english
     if movie.save
-      render json: movie.as_json
+      render json: movie
     else
       render json: { errors: movie.errors.full_messages }, status: :unprocessable_entity
     end
