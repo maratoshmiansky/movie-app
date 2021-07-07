9cb8f871289ee440ce747a7da042a73bcb75aaac
Marat Oshmiansky <maratical@gmail.com>
2021-07-07 07:38:13 -0700

    add has_many and belongs_to associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   has_many :actors  # actors method returns an array of multiple actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, length: { minimum: 1 }
   validates :year, numericality: { only_integer: true, greater_than: 1888 }
--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,4 +2,5 @@ class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :year, :plot, :director, :english
 
   # has_many :actors
+  # has_many :genres
 end
